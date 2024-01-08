450f1ead29762a070ca718da6b91860e6a7fa3fd
Leigh Klotz <klotz@klotz.me>
2024-01-08 12:34:12 -0800

    fix readlink and .gitignore

--- help.sh
+++ help.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname $(readlink "${BASH_SOURCE}"))
+SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE}"))
 
 export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question about Linux, bash, python, or general programming:\n")
 
