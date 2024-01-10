d7f29a98caea447a82e4a163f3dc0abf6526cf79
Leigh Klotz <klotz@klotz.me>
2024-01-09 17:39:49 -0800

    smarten up arg handling and add --stdin option

--- README.md
+++ README.md
@@ -10,18 +10,33 @@
 - summarize.sh
 
 
+## help.sh
+
+USAGE="[-m model-type] [--stdin] [--] QUESTION QUESTION QUESTION"
+
+- `-m`: defaults if not specified; see source for model types
+- `--stdin`: reads stdin and interpolates into prompt; if input is a terminal, asks user for input
+- `--`: unambiguously terminate args
+- `QUESTION`: word or words, can be quoted or not, can be multiline (but also see `--stdin`)
+
+Example:
+```bash
+$ help.sh -m codebooga -- "split bash argument array into left and right with double hyphen as the separator using special bash builtin functions or operators as needed "
+$ help.sh what is my ip address
+```
+
 # LLamafile binaries
 https://github.com/Mozilla-Ocho/llamafile
-- llamafile-0.1.zip
-- llamafile-llava-cli-0.1
-- llamafile-llava-quantize-0.1
-- llamafile-main-0.1
-- llamafile-quantize-0.1
-- llamafile-server-0.1
+- `llamafile-0.1.zip`
+- `llamafile-llava-cli-0.1`
+- `llamafile-llava-quantize-0.1`
+- `llamafile-main-0.1`
+- `llamafile-quantize-0.1`
+- `llamafile-server-0.1`
 
 # Models and all-in-one
 You need to download these from various places on HF and llamafile
-- dolphin-2.5-mixtral-8x7b.Q4_K_M.llamafile
-- llava-v1.5-7b-q4-main.llamafile
-- llava-v1.5-7b-q4-server.llamafile
-- mistral-7b-instruct-v0.1-Q4_K_M-main.llamafile
+- `dolphin-2.5-mixtral-8x7b.Q4_K_M.llamafile`
+- `llava-v1.5-7b-q4-main.llamafile`
+- `llava-v1.5-7b-q4-server.llamafile`
+- `mistral-7b-instruct-v0.1-Q4_K_M-main.llamafile`
--- llm.sh
+++ llm.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-# args, "[-m model-type] [--stdin] QUESTION QUESTION QUESTION"
+USAGE="[-m model-type] [--stdin] [--] QUESTION QUESTION QUESTION"
 
+#
 M="mistral"
-if [[ "${1}" == "-m" ]]; then
-    shift; M="$1"; shift;
-fi
-
 INPUT=""
-if [[ "${1}" == "--stdin" ]]; then
-   if [ -t 0 ]; then
-      echo "Give input followed by Ctrl-D:"
-   fi
-   shift; INPUT=$(cat)
+QUESTION=""
+
+# If there are any args, require "--" or any non-hyphen word to terminate args and start question.
+# Assume the whole args is a question if there's no hyphen to start.
+if [[ "${1}" == "-"* ]]; then
+    for ((index=1; index<$#; index ++)); do
+	arg=${@:$index:1}
+	#echo "index=$index arg=$arg"
+	if [[ "${arg}" == "-m" ]]; then
+	    M="$2"; ((index ++));
+	elif [[ "${arg}" != "-"* ]]; then
+	    QUESTION=("${*:index + 1}")
+	    break
+	elif [[ "${arg}" == "--" ]]; then
+	    QUESTION=("${*:index + 1}")
+	    break
+	elif [[ "${1}" == "--stdin" ]]; then
+	    if [ -t 0 ]; then
+		echo "Give input followed by Ctrl-D:"
+	    fi
+	    INPUT=$(cat)
+	fi
+    done
+else
+    QUESTION="${*}"
 fi
 
-QUESTION="${*}"
+# Use environment variable to change SYSTEM_MESSAGE
 SYSTEM_MESSAGE="${SYSTEM_MESSAGE-$(printf "%b" "Answer the following user question:\n")}"
 
 case "${M}" in
     ## Model: dolphin mxtral 8x7b
     dolphin)
 	MODEL=/home/klotz/wip/llamafiles/dolphin-2.5-mixtral-8x7b.Q4_K_M.llamafile
-PROMPT="<|im_start|>system
+	PROMPT="<|im_start|>system
 ${SYSTEM_MESSAGE}<|im_end|>
 <|im_start|>user
 ${QUESTION}
@@ -42,7 +59,7 @@ ${INPUT}<|im_end|>
 
     ## Model: oobabooga/text-generation-webui/models/codebooga-34b-v0.1.Q4_K_M.gguf
     codebooga)
-	MODEL="/home/klotz/wip/llamafiles/llamafile-main-0.1 -m /home/klotz/wip//oobabooga/text-generation-webui/models/codebooga-34b-v0.1.Q4_K_M.gguf"
+	MODEL="/home/klotz/wip/llamafiles/llamafile-main-0.1 -m /home/klotz/wip/oobabooga/text-generation-webui/models/codebooga-34b-v0.1.Q4_K_M.gguf"
 	PROMPT=$(printf "%b" "[INST]${SYSTEM_MESSAGE}\n${QUESTION}\n${INPUT}[/INST]")
 	NGL=40
 	;;
@@ -56,7 +73,7 @@ ${INPUT}<|im_end|>
 
     ## fail
     *)
-	echo "usage: $0 [-m model-type] question words"
+	echo "usage: $0 ${USAGE}"
 	exit 1
 	;;
 esac
@@ -66,6 +83,6 @@ if ! GPU=$(command -v nvidia-detector) || [[ "$GPU" == "None" ]]; then
 fi
 
 ## Run
-#  -n 1000 ???
-# echo "${PROMPT}"
-${MODEL} --temp 0 -c 6000 -ngl "${NGL}" -p "${PROMPT}" "${SILENT}" 2>/dev/null 
+# -n 1000 ???
+# printf 'Prompt Is %s"' "${PROMPT}"
+printf '%s' "${PROMPT}" | ${MODEL} --temp 0 -c 6000 -ngl "${NGL}" -f /dev/stdin ${SILENT} 2>/dev/null 
