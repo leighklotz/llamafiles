6be16e8f89816d4035751fabefb90f409bd2671c
Leigh Klotz <klotz@klotz.me>
2024-01-09 19:18:14 -0800

    add -c for context length and hardcode model max

--- llm.sh
+++ llm.sh
@@ -8,7 +8,8 @@ QUESTION=""
 ERROR_OUTPUT="/dev/null"
 SILENT_PROMPT="--silent-prompt"
 TEMPERATURE="0"
-CONTEXT_LENGTH="8192"
+CONTEXT_LENGTH=16384
+MAX_CONTEXT_LENGTH=${CONTEXT_LENGTH}
 
 # If there are any args, require "--" or any non-hyphen word to terminate args and start question.
 # Assume the whole args is a question if there's no hyphen to start.
@@ -17,20 +18,22 @@ if [[ "${1}" == "-"* ]]; then
 	arg=${@:$index:1}
 	if [[ "${arg}" == "-m" ]]; then
 	    M="$2"; ((index ++));
+	elif [[ "${arg}" == "-c" ]]; then
+	    CONTEXT_LENGTH="$2"; ((index ++));
 	elif [[ "${arg}" != "-"* ]]; then
 	    QUESTION=("${*:index + 1}")
 	    break
 	elif [[ "${arg}" == "--" ]]; then
 	    QUESTION=("${*:index + 1}")
 	    break
+	elif [[ "${arg}" == "--debug" ]]; then
+	  ERROR_OUTPUT="/dev/stdout"
+	  SILENT_PROMPT=""
 	elif [[ "${arg}" == "--stdin" ]]; then
 	    if [ -t 0 ]; then
 		echo "Give input followed by Ctrl-D:"
 	    fi
 	    INPUT=$(cat)
-	elif [[ "${arg}" == "--debug" ]]; then
-	  ERROR_OUTPUT="/dev/stdout"
-	  SILENT_PROMPT=""
 	fi
     done
 else
@@ -50,13 +53,16 @@ ${SYSTEM_MESSAGE}<|im_end|>
 ${QUESTION}
 ${INPUT}<|im_end|>
 <|im_start|>assistant"
-	NGL=25
+	# NGL=25
+	NGL=20
+	MAX_CONTEXT_LENGTH=32768
 	;;
 
     ## Model: mistral-7b-instruct
     mistral)
 	MODEL=/home/klotz/wip/llamafiles/mistral-7b-instruct-v0.1-Q4_K_M-main.llamafile
 	PROMPT=$(printf "%b" "[INST]${SYSTEM_MESSAGE}\n${QUESTION}\n${INPUT}[/INST]\n")
+	MAX_CONTEXT_LENGTH=8000
 	NGL=33
 	;;
 
@@ -65,6 +71,7 @@ ${INPUT}<|im_end|>
 	MODEL="/home/klotz/wip/llamafiles/llamafile-main-0.1 -m /home/klotz/wip/oobabooga/text-generation-webui/models/codebooga-34b-v0.1.Q4_K_M.gguf"
 	PROMPT=$(printf "%b" "[INST]${SYSTEM_MESSAGE}\n${QUESTION}\n${INPUT}[/INST]\n")
 	SILENT_PROMPT=""	# not supported by codebooga
+	MAX_CONTEXT_LENGTH=16384
 	NGL=40
 	;;
 
@@ -77,6 +84,11 @@ esac
 
 if ! GPU=$(command -v nvidia-detector) || [[ "$GPU" == "None" ]]; then
     NGL=0
+fi
+
+if [ "$CONTEXT_LENGTH" -gt "$MAX_CONTEXT_LENGTH" ]; then
+    CONTEXT_LENGTH="$MAX_CONTEXT_LENGTH"
+    echo "* Truncated context length to $CONTEXT_LENGTH"
 fi
 
 ## Run
