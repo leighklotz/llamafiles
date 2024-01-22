e57f6496434dd34462d09acb2e3a1f172273048c
Leigh Klotz <klotz@klotz.me>
2024-01-22 13:34:28 -0800

    make prompt less strict

--- help.sh
+++ help.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
-export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question about Linux, bash, python, or general programming:\n")
+export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question about Linux, Bash, Python, general programming, or even other subjects:\n")
 
 exec ${SCRIPT_DIR}/llm.sh ${*}
