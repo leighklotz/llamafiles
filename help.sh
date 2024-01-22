50b9d106b9ea7c0fd36b0dc99bced7fca1afea55
Leigh Klotz <klotz@klotz.me>
2024-01-22 15:10:06 -0800

    help.sh prompt: open world hypothesis

--- help.sh
+++ help.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
-export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question about Linux, Bash, Python, general programming, or even other subjects:\n")
+export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question, or follow the user instruction, about Linux, Bash, Python, general programming, or even other subjects, if you know the answer:\n")
 
 exec ${SCRIPT_DIR}/llm.sh ${*}
