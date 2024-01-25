c48584363f3e00a35e8feebfe151dcbeb0f017df
Leigh Klotz <klotz@klotz.me>
2024-01-25 10:55:55 -0800

    protect args better, still problematic when I can't use exec

--- help.sh
+++ help.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
 export SYSTEM_MESSAGE=$(printf "%b" "Answer the following user question, or follow the user instruction, about Linux, Bash, Python, general programming, or even other subjects, if you know the answer:\n")
 
-exec ${SCRIPT_DIR}/llm.sh
+exec ${SCRIPT_DIR}/llm.sh "$@"
+
