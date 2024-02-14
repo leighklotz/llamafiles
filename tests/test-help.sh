18f32887602a6085abe165bc5eebe096f8e1cd9d
Leigh Klotz <klotz@klotz.me>
2024-02-14 15:07:16 -0800

    better test script

--- tests/test-help.sh
+++ tests/test-help.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE}"))
 
-SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
+# This script reads lists of questions and model types from files, and
+# uses the `help.sh` script to test each question with each model type.
 
-while IFS= read -r q
+
+# Test Configuration
+MODEL_TYPES_FILE="${SCRIPT_DIR}/../models/model-types.txt"
+QUESTIONS_FILE=${SCRIPT_DIR}/help-questions.txt
+
+# Read  line  by line and store each line in an array
+declare -a questions
+declare -a model_types
+IFS=$'\n' read -ra questions < "${QUESTIONS_FILE}"
+IFS=$' ' read -ra model_types < "${MODEL_TYPES_FILE}"
+
+for q in "${questions[@]}"
 do
     echo "=== ${q} ==="
-    for m in $(cat ${SCRIPT_DIR}/../models/model-types.txt)
+    for m in "${model_types[@]}"
     do
 	echo -n "${m} "
-	time help.sh -m "${m}" "${q}"
+	time help.sh --speed -m "${m}" "${q}"
 	echo ""
     done
-echo ""
-done < ${SCRIPT_DIR}/questions.txt
+    echo ""
+done
+
