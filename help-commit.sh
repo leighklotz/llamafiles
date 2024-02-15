b921b5dbf8f34745bcbc38eab7e87409d85e94ca
Leigh Klotz <klotz@klotz.me>
2024-02-14 22:16:48 -0800

    help-commit.sh

--- help-commit.sh
+++ help-commit.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-code.sh git git diff | help.sh 'Output a one-line `git commit -m` line that will commit these changes:'
+staged=""
+
+output="$(git diff)"
+if [ "${output}" == '' ]; then
+    echo NO OUTPUT LOOKING FOR STAGED
+    output="$(git diff --staged)"
+    staged=" staged "
+fi
+
+if [ "${output}" == '' ]; then
+    echo no changes seen
+    exit 1
+fi
+
+code.sh git printf "%s" "${output}" | help.sh "Output a one-line \`git commit -m\` line that will commit these${staged}changes:"
