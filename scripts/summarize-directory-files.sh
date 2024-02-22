14710e61313eeb9906a5a4d4ee1a602eacbd49f1
Leigh Klotz <klotz@klotz.me>
2024-02-21 16:22:27 -0800

    summarize-directory-files.sh: nlines

--- scripts/summarize-directory-files.sh
+++ scripts/summarize-directory-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
-
 GBNF_FILE=$(mktemp -t sumdir-XXXXXX.gbnf)
+NLINES=100
 
 cat > ${GBNF_FILE} <<EOF
 root ::= "[" [A-Za-z][^\]]+ "]" "(" [^)]+ ")"
@@ -13,11 +13,14 @@ echo "# Files in $(basename $(pwd))"
 
 for FN in *
 do
-    PROMPT="For the file named ${FN} whose first few lines are shown below, generate a title and output '[title](${FN})' markdown link with link text being a short title of the file."
-    echo -n "- "
-    ${SCRIPT_DIR}/code.sh ${FN} head -5 ${FN} | \
-	${SCRIPT_DIR}/help.sh ${*} --grammar-file ${GBNF_FILE} \
-          -e -- "${PROMPT}"
+    if [ -f "${FN}" ] ;
+    then
+	PROMPT="For the file named ${FN} whose first ${NLINES} lines are shown below, generate a title and output '[title](${FN})' markdown link with link text being a short title of the file."
+	echo -n "- "
+	${SCRIPT_DIR}/code.sh ${FN} head -${NLINES} ${FN} | \
+	    ${SCRIPT_DIR}/help.sh ${*} --grammar-file ${GBNF_FILE} \
+	      -e -- "${PROMPT}"
+    fi
 done
 
 rm "${GBNF_FILE}"
