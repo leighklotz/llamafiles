25eba73743ac21c2ca26a3bfba195f59b966ed99
Leigh Klotz <klotz@klotz.me>
2024-02-26 20:42:09 -0800

    mixtral: lower NGL to 22
    - todo: NGL and gpu_check are per model-type but should be per model

--- models/mixtral/functions.sh
+++ models/mixtral/functions.sh
@@ -26,7 +26,7 @@ function mixtral_priority {
     MAX_CONTEXT_LENGTH=21000
     case "${PRIORITY}" in
          speed)
-             NGL=${NGL:=23}
+             NGL=${NGL:=22}
              CONTEXT_LENGTH=2048
              ;;
          length)
@@ -34,7 +34,7 @@ function mixtral_priority {
              CONTEXT_LENGTH=${MAX_CONTEXT_LENGTH}
              ;;
          manual)
-             NGL=${NGL:=23}
+             NGL=${NGL:=22}
              CONTEXT_LENGTH=${CONTEXT_LENGTH:=4096}
              ;;
          *)
