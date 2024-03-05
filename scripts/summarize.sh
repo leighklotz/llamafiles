d35dae992bf7c76a26b9958b9993b164002fbee0
Leigh Klotz <klotz@klotz.me>
2024-03-05 11:35:57 -0800

    summarize: arg parsing

--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -2,19 +2,25 @@
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
-URL="${!#}"			# LAST
-ARGS="${@:1:${#}-1}"		# BUTLAST
+URL=${1:-}			# LAST
+ARGS=${@:2}			# BUTLAST
 
-if LYNX=$(command -v lynx); then
-   CMD="${LYNX} --dump --nolist"
-elif LINKS=$(command -v links); then
-   CMD="${LINKS} -codepage utf-8 -force-html -width 72 -dump"
+if [ -z "$URL" ]; then
+    echo "Usage: $(basename $0) <URL> [llm.sh options]"
+    exit 1
+fi
+
+if command -v lynx &> /dev/null; then
+    CMD="lynx --dump --nolist"
+elif command -v links &> /dev/null; then
+    CMD="links -codepage utf-8 -force-html -width 72 -dump"
 else
-   echo "error: NOLINKS"
-   exit 1
+    echo "error: NOLINKS"
+    exit 1
 fi
 
 export SYSTEM_MESSAGE=$(printf "%b" "Summarize the following web page article and ignore website header at the start and look for the main article:\n#### Text of ${URL}\n###\n")
 
 # somehow need to protect ${ARGS} better
-${CMD} "${URL}" | ${SCRIPT_DIR}/llm.sh --length ${ARGS}
+${CMD} "${URL}" | ${SCRIPT_DIR}/llm.sh --length "${ARGS}"
+
