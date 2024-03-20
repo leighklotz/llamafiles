01edab97cef883799df3856eb93df6f9ceccd8c0
Leigh Klotz <klotz@klotz.me>
2024-03-20 15:16:15 -0700

    nous-hermes: accomodate models

--- models/nous-hermes/functions.sh
+++ models/nous-hermes/functions.sh
@@ -35,8 +35,8 @@ function nous_hermes_priority {
 
 function nous_hermes_model {
     MODEL=$(find_first_model \
-                ${MODELS_DIRECTORY}/mixtral/mixtral-8x7b-instruct-v0.1.Q5_K_M.llamafile \
-                ${MODELS_DIRECTORY}/mixtral/mixtral_7bx2_moe.Q3_K_M.gguf \
+                ${MODELS_DIRECTORY}/nous-hermes/nous-hermes-2-mixtral-8x7b-dpo.Q5_K_M.gguf \
+                ${MODELS_DIRECTORY}/nous-hermes/Nous-Hermes-2-Mixtral-8x7B-DPO.Q4_K_M.gguf \
          )
     gpu_check 1
     nous_hermes_prompt
