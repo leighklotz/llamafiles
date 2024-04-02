ec128c24a73872cbd724c858771a02b1f97c8120
Leigh Klotz <klotz@klotz.me>
2024-04-02 14:36:39 -0700

    summarize.sh/scuttle.sh: make more resilient, still has JSON weakness

--- scripts/summarize.sh
+++ scripts/summarize.sh
@@ -7,6 +7,9 @@ ARGS=${@:2}			# BUTLAST
 
 post_process=""
 
+# fixme
+. ${SCRIPT_DIR}/../models/functions.sh
+
 if [ -z "$LINK" ]; then
     echo "Usage: $(basename $0) <LINK> [llm.sh options]"
     exit 1
@@ -38,20 +41,37 @@ esac
 
 export SYSTEM_MESSAGE=$(printf "%b" "${SYSTEM_MESSAGE}")
 
+# transforms JSON output from LLM into a properly formatted URL string for Scuttlebookmark adding.
+# todo: try to find the json in the input, for example inside backquotes
+#       or force json schema
+function scuttle_extract_json {
+    jq --arg xspace "%20" --arg plus "+" --arg xcomma "%2[cC]" --arg comma "," -r '.keywords |= if(type == "array") then join(",") else . end | "https://scuttle.klotz.me/bookmarks/klotz?action=add&address=\(.link|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&description=\(.description|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&title=\(.title|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&tags=\(.keywords|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))"'
+    return $?;
+}
+
 function post_process {
     case "$post_process" in
 	summarize)
 	;;
 	scuttle)
 	    # <https://scuttle.klotz.me/bookmarks/klotz?action=add&address=https://example.com&title=Example+Website+&description=This+is+an+example+website&tags=example,website,canonical+page>
-	    jq --arg xspace "%20" --arg plus "+" --arg xcomma "%2[cC]" --arg comma "," -r '.keywords |= join(",") | "https://scuttle.klotz.me/bookmarks/klotz?action=add&address=\(.link|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&description=\(.description|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&title=\(.title|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&tags=\(.keywords|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))"'
-	;;
+	    if [ -n "$VERBOSE" ]; then
+		log_debug "post_process"
+		tee /dev/stderr | scuttle_extract_json
+		s=$?
+	    else
+		scuttle_extract_json
+		s=$?
+	    fi
+	    [ $s != 0 ] && log_and_exit $s "scuttle post_process"
+	    return $s
+	    ;;
 	*)
-	    echo "* $0: unknown post_process" >> /dev/stderr
-	    exit 1
+	    log_error 1 "unknown post_process"
     esac
 }
 
 ${LYNX} "${LINK}" | ${SCRIPT_DIR}/llm.sh --length ${ARGS} "# Text of link ${LINK}" | post_process
 
 # todo: need to protect ${ARGS} better
+
