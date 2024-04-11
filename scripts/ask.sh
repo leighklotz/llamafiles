221dc69d8296e504ead9de80d14d690c588cc5d9
Leigh Klotz <klotz@klotz.me>
2024-04-10 22:23:07 -0700

    ask.sh: split out system message and start with the minimal 'Follow the user instruction:'

--- scripts/ask.sh
+++ scripts/ask.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
+ASK_SYSTEM_MESSAGE="$(printf "%b" "Follow the user instruction:")"
+export SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-${ASK_SYSTEM_MESSAGE}}"
+
 MODEL_TYPE=$1
 shift
 exec ${SCRIPT_DIR}/help.sh -m "${MODEL_TYPE}" "$@"
