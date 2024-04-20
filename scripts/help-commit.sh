888f87bb99182e6e286236201a7094478b9164f6
Leigh Klotz <klotz@klotz.me>
2024-04-19 18:48:58 -0700

    help-commit: add ---help; slight bashisms

--- scripts/help-commit.sh
+++ scripts/help-commit.sh
@@ -8,12 +8,19 @@ MESSAGE_LINE="oneline"
 export MODEL_TYPE="${MODEL_TYPE:=mixtral}"
 
 function usage() {
-    echo "help-commit.sh [git diff options] -- [help.sh options]"
+    p=$(basename "$0")
+    echo "$p: [git diff options] -- [help.sh options]"
+    echo "- change model with \`$p -- -m model\` or \`export \$MODEL_TYPE=$MODEL_TYPE\`"
+    #set -x
 }
 
 # help-commit.sh [git diff options] -- [help.sh options]
 while [[ $# -gt 0 ]]; do
     case $1 in
+	--help)
+	    usage
+	    exit 0
+	    ;;
 	--oneline|--multiline|--one-line|--multi-line)
 	    MESSAGE_LINE=$(echo "$1" | sed -E -e 's/-//g')
 	    shift
@@ -30,40 +37,44 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-m="$(printf "%b" "You are an expert in Linux, Bash, Python, general programming, and related topics.\n")"
-export SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-${m}}"
+# Set SYSTEM_MESSAGE, PROMPT and GRAMMAR_FILE
+default_system_message="$(printf "%b" "You are an expert in Linux, Bash, Python, general programming, and related topics.\n")"
+export SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-${default_system_message}}"
+
 printf -v PROMPT 'Explain the following git diff as a %s git commit message, expressed in the form of a \`git commit\` command.\n' "${MESSAGE_LINE}"
+
 GRAMMAR_FILE_FLAG="--grammar-file ${SCRIPT_DIR}/git-commit-${MESSAGE_LINE}-grammar.gbnf"
+
+# Pipeline to connect 'git diff' with 'help.sh' below.
 
 function get_results {
     # set globals
-    staged=" $1 "
-    diff_command="git diff $staged ${GIT_DIFF_OPTIONS}"
-    diff_output="$($diff_command)"
+    local options=" $1 "
+    DIFF_COMMAND="git diff $options ${GIT_DIFF_OPTIONS}"
+    DIFF_OUTPUT="$($DIFF_COMMAND)"
 }
 
-get_results --staged
+get_results ""
 
-if [ "${diff_output}" == '' ]; then
+if [ "${DIFF_OUTPUT}" == '' ]; then
     echo "No staged changes, looking for unstaged" >> /dev/stderr
-    get_results ""
+    get_results --staged
 fi
 
-if [ "${diff_output}" == '' ]; then
+if [ "${DIFF_OUTPUT}" == '' ]; then
     echo "No changes seen" >> /dev/stderr
     exit 1
 fi
 
 # remove triple-backquote from the diff output since we're enclosing the body in that
-diff_output_sanitized="$(printf "%s" "$diff_output" | sed -e 's/```/`_`_`/g')"
+diff_output_sanitized="$(printf "%s" "$DIFF_OUTPUT" | sed -e 's/```/`_`_`/g')"
 
-# uses current default model, e.g. $MODEL_TYPE
 #set -x
 printf -v INPUT '```sh
 $ %s
 %s
 ```\n' \
-       "${diff_command}" "${diff_output_sanitized}"
+       "${DIFF_COMMAND}" "${diff_output_sanitized}"
 
 # printf "%s\n" "${INPUT}"
 
