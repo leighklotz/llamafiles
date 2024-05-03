ebb070e94f450523aa66535010489a6d7f64d34d
Leigh Klotz <klotz@klotz.me>
2024-05-03 11:57:03 -0700

    scripts: llava script fix

--- scripts/image-name.sh
+++ scripts/image-name.sh
@@ -2,9 +2,6 @@
 
 IMG="${1}"
 
-~klotz/wip/llamafiles/models/llava-v1.5-7b-q4-main.llamafile \
-    --image "${1}" --temp 0 -ngl 35 \
-    --grammar 'root ::= [a-z]+ (" " [a-z]+)+' -n 16 \
-    -e -p '### User: The name of this image is ...\n### Assistant:' \
-    --silent-prompt 2>/dev/null | \
+LLAVA=~/wip/llamafiles/models/llava-v1.5-7b-q4.llamafile
+${LLAVA} --image "${1}" --temp 0 -ngl 35 --grammar 'root ::= [a-z]+ (" " [a-z]+)+' -n 16 -e -p '### User: The name of this image is ...\n### Assistant:' --silent-prompt 2>/dev/null | \
     sed -e's/ /_/g' -e's/$/.jpg/'
