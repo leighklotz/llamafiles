85c5a1b15d05e0c83cb53cdc4c16ab0d2142911e
Leigh Klotz <klotz@klotz.me>
2024-05-11 10:34:41 -0700

    scripts/nvfree.sh   - Updated the script to check for nvidia-smi command first and fail gracefully if not found   - Use `#!/bin/bash -e` to enable exit on error   - Added `set -o pipefail` to handle errors in pipe operations

--- scripts/nvfree.sh
+++ scripts/nvfree.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
-nvidia-smi --query-gpu=memory.free --format=csv,nounits,noheader | awk '{printf "%.2f GiB", $1 / 1024}'
-echo ""
+set -o pipefail
+if command -v nvidia-smi &> /dev/null;
+   then
+       nvidia-smi --query-gpu=memory.free --format=csv,nounits,noheader | awk '{printf "%.2f GiB", $1 / 1024}'
+       echo ""
+else
+    echo "0"
+    exit 1
+fi
