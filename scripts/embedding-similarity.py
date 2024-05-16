70eab9e014625e6b909b96d45da5d79dd1c70878
Leigh Klotz <klotz@klotz.me>
2024-05-16 13:49:38 -0700

    embedding-similarity.py: second draft

--- scripts/embedding-similarity.py
+++ scripts/embedding-similarity.py
@@ -21,11 +21,9 @@ def calculate_embedding_for_text(text):
     url = "http://localhost:5000/v1/embeddings"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer no-key"
     }
     data = {
         "input": text,
-        "model": "GPT-4",
         "encoding_format": "float"
     }
 
@@ -59,4 +57,19 @@ def calculate_embedding(file_name):
 
 
 def calculate_embeddings(directory_name):
-    return { file_name : calculate_embeddings(file_name) for file_name in os.listdir(directory_name) }
+    return { file_name : calculate_embedding(file_name) for file_name in os.listdir(directory_name) }
+
+def main(directory_name):
+    file_embeddings = calculate_embeddings(directory_name))
+    # todo: calculate cosine distance for all pairs
+    cosine_distance_matrix = calculate_cosine_distance_matrix(file_embeddings)
+    # todo: print cosine_distance_matrix as a table
+    print_cosine_distance_matrix(cosine_distance_matrix)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        directory_name = sys.argv[1]
+        main(directory_name)
+    else:
+        print("usage: {sys.argv[0]} dirname")
+        sys.exit(1)
