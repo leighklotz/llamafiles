f7825e9ffbb2977f77941591ea32eccbb6eb8400
Leigh Klotz <klotz@klotz.me>
2024-05-29 17:10:38 -0700

    scuttle.sh: --json and --help

--- scripts/scuttle.sh
+++ scripts/scuttle.sh
@@ -2,19 +2,45 @@
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE}"))
 
-LINK=${1:-}			# LAST
-ARGS=${@:2}			# BUTLAST
-
 . ${SCRIPT_DIR}/../models/functions.sh
 
-if [ -z "$LINK" ]; then
-    echo "Usage: $(basename $0) <LINK> [llm.sh options]"
+function usage() {
+    echo "Usage: $(basename $0) [--json] <LINK> [llm.sh options]"
     exit 1
+}
+
+JSON_MODE=""
+
+while true;
+do
+    case "$1" in
+	"--help")
+	    usage
+	    exit 1
+	    ;;
+	"--json")
+	    JSON_MODE=1
+	    shift
+	    ;;
+	*)
+	    LINK=$1
+	    ARGS=$@
+	    break
+	    ;;
+    esac
+done
+
+
+if [ -z "$LINK" ];
+then
+    usage
 fi
 
-if command -v lynx &> /dev/null; then
+if command -v lynx &> /dev/null;
+then
     LYNX="lynx --dump --nolist"
-elif command -v links &> /dev/null; then
+elif command -v links &> /dev/null;
+then
     LYNX="links -codepage utf-8 -force-html -width 72 -dump"
 else
     echo "error: NOLINKS"
@@ -28,13 +54,20 @@ export SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-$(printf "%b" "${SCUTTLE_SYSTEM_MESSAGE
 # todo: try to find the json in the input, for example inside backquotes
 #       or force json schema
 function scuttle_extract_json {
-    jq --arg xspace "%20" --arg plus "+" --arg xcomma "%2[cC]" --arg comma "," -r '.keywords |= if(type == "array") then join(",") else . end | "https://scuttle.klotz.me/bookmarks/klotz?action=add&address=\(.link|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&description=\(.description|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&title=\(.title|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&tags=\(.keywords|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))"'
-    return $?;
+    if [ -n "${JSON_MODE}" ];
+    then
+	cat
+	return $?
+    else
+	jq --arg xspace "%20" --arg plus "+" --arg xcomma "%2[cC]" --arg comma "," -r '.keywords |= if(type == "array") then join(",") else . end | "https://scuttle.klotz.me/bookmarks/klotz?action=add&address=\(.link|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&description=\(.description|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&title=\(.title|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))&tags=\(.keywords|@uri|gsub($xspace; $plus)|gsub($xcomma; $comma))"'
+	return $?
+    fi
 }
 
 function post_process {
     # <https://scuttle.klotz.me/bookmarks/klotz?action=add&address=https://example.com&title=Example+Website+&description=This+is+an+example+website&tags=example,website,canonical+page>
-    if [ -n "$VERBOSE" ]; then
+    if [ -n "$VERBOSE" ];
+    then
 	log_debug "post_process"
 	tee /dev/stderr | scuttle_extract_json
 	s=$?
