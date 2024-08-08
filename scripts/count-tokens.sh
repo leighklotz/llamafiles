e96900db585c62effaa5f94a438c96bd31910934
Leigh Klotz <klotz@klotz.me>
2024-08-08 13:36:29 -0700

    count-tokens.sh: improve readability and performance

--- scripts/count-tokens.sh
+++ scripts/count-tokens.sh
@@ -4,5 +4,7 @@
 
 URL=http://localhost:5000/v1/internal/token-count
 text="$(cat $1)"
-json=$(jq -n --arg text "$text" '{ text: $text }')
-curl -s -X POST -H "Content-Type: application/json" -d "$json" $URL | jq '.length'
+printf -v json '{ "text": "%s" }' "${text//\"/}"
+printf "%d\n" "$((${#json}/4))"
+printf "%s\n" "${json}" 
+printf "%s\n" "${json}" | curl -s -X POST -H "Content-Type: application/json" -d @- $URL | jq '.length'
