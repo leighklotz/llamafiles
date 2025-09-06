5efb287995b2d32b0bfd42785f03b5fc8242977d
Leigh Klotz <klotz@klotz.me>
2025-09-06 12:56:28 -0700

    update prompt in create-project-history

--- create-project-history.sh
+++ create-project-history.sh
@@ -1,9 +1,6 @@
 #!/bin/bash -x
 
-#(bashblock cat README.md; bashblock git log --reverse) | help.sh "write a project_history.md file for the llamafiles project. Use the git log output as source if truth, and breakninto major sections and themes. Use ideqs and terms from the README. Folloenthe timelinr, stsrting Jan 5, 2024" > m-r-$$.md
-
-#PMT="write a project_history.md file for the llamafiles project. Use the git log output as source if truth, and breakninto major sections and themes. Use ideqs and terms from the README. Folloenthe timelinr, stsrting Jan 5, 2024"
-PMT='Please write a project_history.md file for the llamafiles project. Use the git log output as source of truth, and break into major sections and themes. Use ideas and terms from the README. Identify major features and subsystems and document their history. Group conmits together by theme instead of explaining each commit. Finish with an overall narrative of development themes, subsystems, and features.'
+PMT='Please write a project_history.md file for the llamafiles project. Use the git log output as source of truth, and break into major sections and themes. Use full m onth/day/year for dates. Use ideas and terms from the README. Identify major features and subsystems and document their history. Group conmits together by theme instead of explaining each commit. Finish with an overall narrative of development themes, subsystems, and features.'
 pm="$(printf "%s" "${PMT}" | md5sum | awk '{print $1}')"
 
 (bashblock cat README.md; bashblock git log --reverse) | help.sh "${PMT}" > m-r--$pm-$$.md
