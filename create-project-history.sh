db9129ac6d0e6fc543de1c6bf97cbcc726100396
Leigh Klotz <klotz@klotz.me>
2025-09-06 13:09:58 -0700

    create-project-history: remove project name

--- create-project-history.sh
+++ create-project-history.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-PMT='Please write a project_history.md file for the llamafiles project. Use the git log output as source of truth, and break into major sections and themes. Use full month/day/year for dates. Use ideas and terms from the README. Identify major features and subsystems and document their history. Group conmits together by theme instead of explaining each commit. Finish with an overall narrative of development themes, subsystems, and features.'
+PMT='Please write a project_history.md file for the git project below. Use the git log output as source of truth, and break into major sections and themes. Use full month/day/year for dates. Use ideas and terms from the README. Identify major features and subsystems and document their history. Group conmits together by theme instead of explaining each commit. Finish with an overall narrative of development themes, subsystems, and features.'
 pm="$(printf "%s" "${PMT}" | md5sum | awk '{print $1}')"
 
 #(bashblock cat README.md; bashblock git log --reverse) | help.sh "${PMT}" > m-r--$pm-$$.md
