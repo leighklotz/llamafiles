7553817e28a8b6484397766fe5084fbb6cbcfcc5
Leigh Klotz <klotz@klotz.me>
2025-09-19 10:05:30 -0700

    Fix: Corrected comments in llamafiles-aliases.sh

--- scripts/llamafiles-aliases.sh
+++ scripts/llamafiles-aliases.sh
@@ -1,10 +1,10 @@
-# # use `builtin help` if you want native bash help command
+# use `builtin help` if you want native bash help command
 help () 
 { 
     help.sh "$@"
 }
 
-# # use `/usr/bin/write` if you want to chat
+# use `/usr/bin/write` if you want to chat
 write () 
 { 
     write.sh "$@"
