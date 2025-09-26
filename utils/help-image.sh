2a4d169b520b04efcf9b4efa828004f11ab522b7
Leigh Klotz <klotz@klotz.me>
2025-09-26 10:38:34 -0700

    utils: move some ancillary scripts here

--- utils/help-image.sh
+++ utils/help-image.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
 
 [ -z "${IN_LLM_SH_ENV}" ] && [ -f "${SCRIPT_DIR}/env.sh" ] && source "${SCRIPT_DIR}/env.sh"
 
-. ${SCRIPT_DIR}/.venv/bin/activate
+[ -f ${SCRIPT_DIR}/.venv/bin/activate ] && source ${SCRIPT_DIR}/.venv/bin/activate
 
 exec ${SCRIPT_DIR}/help-image.py "$@"
