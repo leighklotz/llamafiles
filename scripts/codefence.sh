2f46adbc3d0db8589d917cf9ba7504be54ffcb11
Leigh Klotz <klotz@klotz.me>
2025-10-24 13:06:13 -0700

    Add logic to codefence.sh + improve filetype mapping in filetype.sh

--- scripts/codefence.sh
+++ scripts/codefence.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -e
 
-printf '```bash
-$ %s\n' "${*}"
-${*}
-s=$?
-printf '```\n'
-exit $?
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
+
+fn="$1"
+
+if [ -z "${fn}" ]; then
+    echo "usage: $0 filename"
+    exit 1
+fi
+
+if [ ! -r "${fn}" ]; then
+    echo "cannot read ${fn}"
+    exit 1
+fi
+
+ft="$("$SCRIPT_DIR/filetype.sh" "${fn}")"
+printf '```%s\n%s\n```\n' "${ft}" "$(cat "${fn}")"
