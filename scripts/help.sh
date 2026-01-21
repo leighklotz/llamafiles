186c380b39ad79ffa3e1305347cfe321dbf0a43a
Leigh Klotz <klotz@klotz.me>
2026-01-21 11:18:36 -0800

    help.sh: add --process-question-escapes

--- scripts/help.sh
+++ scripts/help.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE}")")"
 LINUX_HELP_SYSTEM_MESSAGE="$(printf "%b" "You are an AI bot designed to assist with various tasks, including answering questions, providing information, and executing commands. You can help you with Linux, Bash, Python, general programming, and other subjects. Our interactions are one-shot, question and response. Answer the following user question, write the requested code, or follow other user instruction, about Linux, Bash, Python, general programming, or even other subjects, if you know the answer:\n")"
 export SYSTEM_MESSAGE="${SYSTEM_MESSAGE:-${LINUX_HELP_SYSTEM_MESSAGE}}"
 
-exec "${SCRIPT_DIR}/llm.sh" "$@"
-#exec "${SCRIPT_DIR}/llm.sh" "${@}"
+exec "${SCRIPT_DIR}/llm.sh" --process-question-escapes "$@"
+
